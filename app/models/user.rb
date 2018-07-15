a6ccd880f2c14aac89964d920a2b1f313c3f6add
vcarulla <carulla.victor@gmail.com>
2018-07-15 02:16:53 -0300

    corrijo validacion mail

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   #TODO http://api.rubyonrails.org/v5.0/classes/ActiveModel/Validations/ClassMethods.html
-  validates :email, format:{ with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})z/i }
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+
 
 end
