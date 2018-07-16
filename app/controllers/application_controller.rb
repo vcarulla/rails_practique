0afe219d96e1a44151b036d9a0a7697fb9478a18
vcarulla <carulla.victor@gmail.com>
2018-07-16 10:15:26 -0300

    Uso de Memorization para hacer uso del cache de objetos en los pre-request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,11 @@
 class ApplicationController < ActionController::Base
   # Prevents CSRF attacks by raising an exception.
   # For API's, you may want to use :null_session instead.
-  # protect_from_forgery with: :exception
+  protect_from_forgery with: :exception
 
+  #TODO @current_user ||= to use memoization in your application, to cache objects on a per-request basis. This way your application will only load the object only once.
   def current_user
-    User.where(id: session[:user_id]).first
+    @current_user ||= User.where(id: session[:user_id]).first
   end
 
   helper_method :current_user
