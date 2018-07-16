04e174e2297d73455df270489faf9986e4e19d98
vcarulla <carulla.victor@gmail.com>
2018-07-16 11:07:32 -0300

    Manejo de rutas

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users
+  resources :users, only: [:create]
 
-  resource :session
+  resource :session, only: [:create]
+
+  #TODO User Friendly Url's
+  get 'login'     => 'sessions#new'
+  delete 'logout' => 'sessions#destroy'
+  get 'register'  => 'users#new'
 
   resources :posts do
     resources :comments
