f607e3c6e7740366471f9a1fc6eaf53fe3817c0b
vcarulla <carulla.victor@gmail.com>
2018-07-16 16:11:22 -0300

    Flash messages en login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,9 @@ class ApplicationController < ActionController::Base
   # For API's, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  #TODO se agregan los tipos de flash(error msh) con las mismas claves que los nombres de clases alerts de bootstrap
+  add_flash_types :danger, :info, :warning, :success
+
   #TODO @current_user ||= to use memoization in your application, to cache objects on a per-request basis. This way your application will only load the object only once.
   def current_user
     @current_user ||= User.where(id: session[:user_id]).first
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,10 @@ class SessionsController < ApplicationController
     @user = User.where(email: params[:email]).first
     if @user.confirmed? and @user.authenticate(params[:password])
       session[:user_id] = @user.id
-      redirect_to root_path
+      redirect_to root_path, success: 'You have signed in!'
     else
-      flash[:error] = "Invalid email or password"
-      render :new
+      # flash[:error] = "Invalid email or password"
+      redirect_to login_path, danger: 'Invalid email or password'
     end
   end
 
