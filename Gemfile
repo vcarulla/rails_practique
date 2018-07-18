d5fd11f26078e92b05badd8ef5cb642b17d1e917
vcarulla <carulla.victor@gmail.com>
2018-07-17 23:02:50 -0300

    Implement estado de post publicado/borrador

--- Gemfile
+++ Gemfile
@@ -36,6 +36,8 @@ gem 'bcrypt', '~> 3.1.7'
 gem 'bootstrap-sass'
 #Pagination
 gem 'kaminari'
+# Active record enum
+gem 'stateful_enum'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.1.0', require: false
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,16 @@
 class Post < ApplicationRecord
+
+  # Active record enum gem 'stateful_enum'
+  enum status: {draft: 0, published: 1} do
+    event :publish do
+      after do
+        self.published_at = DateTime.now
+        self.save
+      end
+      transition :draft => :published
+    end
+  end
+
   #TODO has_many es un macro que se encarga de hacer la magia para que este modelo haga referencia a otros modelos (one to many relasonship)
   has_many :comments
 
